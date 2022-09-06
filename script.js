73e6996b700c12c77bc73d45a7aeb373e9cc59bf
Matheus <22000500@aluno.cotemig.com.br>
2022-09-05 21:53:40 -0300

    Resultado e Tempo Esgotado

--- script.js
+++ script.js
@@ -2,16 +2,31 @@
 $(function(){ //DEFININDO O JQUERY
     
     
+    //FUNÇÕES
     
     
 
-
-    //funções
-    
     //receber resposta do usuario e calcular
-    $("#btn").click(function(){
-        const usuarioRes = Number($("#resposta").val())
-        
+        $("#formBtn").click(function(){
+            let resUser = $('#resposta').val() //recebendo na variavel o valor digitado no input
+            let certo = false; //certo declarado em falso padrao para encurtar o if removendo nescessidade de else
+            
+            if(evaluate(num1, num2, operador) == resUser){
+                certo = true
+            }
+
+          
+            //alerta que exibe resultado
+            if(certo)
+            {
+                    alert('PARABENS, VOCE ESTÁ CERTO')
+            }
+
+              
+            //consoleLog para teste
+              console.log("Numero Digitado:", resUser)
+              console.log("Usuario certo?:", certo)
+  
         })
 
 
@@ -23,7 +38,6 @@ $(function(){ //DEFININDO O JQUERY
     setInterval(() => {
         //contador
         
-       
         let segundoAtual = Number($(".countdown span").html()); //recebe no segundoatual o valor da tag span
         if(segundoAtual > 0) //só irá alterar o valor do segundo se ele for maior que 0
         {
@@ -33,56 +47,46 @@ $(function(){ //DEFININDO O JQUERY
         else
         {
             $(".resultado h1").html('TEMPO ESGOTADO!')
+            
+            $("#resposta").remove(); //comando do jquery que remove um elemento por inteiro
+
+            $("#formBtn").html('FIM') //altera o botao para fim
         }
+
+
     }, 1000);
 
+    
+    
     //fazer submit nao reiniciar a pagina
-    $("#wpResposta form").submit(function(e) {
-        e.preventDefault();
-    });
+         $("#wpResposta form").submit(function(e) {
+                 e.preventDefault();
+         });
 
-    //operadores aleatorios
+
+
+    //operadores aleatorios (fiz de cabeça, to bem orgulhoso de mim)
     
     let operaVetor = ["+", "-", "/", "*"] //vetor com operadores
     let i = Math.floor((Math.random() * 4) + 0); //indice aleatorio de 0 a 4
 
-    let num1 = Math.floor((Math.random() * 100) + 1); //numero aleatório de 1 a 100
-    let num2 = Math.floor((Math.random() * 100) + 1); //numero aletorio de 1 a 100
+    let num1 = Math.floor((Math.random() * 25) + 5); //numero aleatório de 5 a 25
+    let num2 = Math.floor((Math.random() * 10) + 1); //numero aletorio de 1 a 10
     let operador = operaVetor[i];   //operador aleatório
     
 
-    $("#num1").html(num1) 
+    $("#num1").html(num1) //aplicando na tela
     $("#num2").html(num2)
     $("#operador").html(operador)
 
-    //resultado
+    //função para calcular resultado
     function evaluate(param1, param2, operator) {
         return eval(param1 + operator + param2);
       }
-
-    
-      
-
-
-
 
     //CONSOLE LOGS PARA TESTE
     console.log("Operador: ",operador)  
     console.log("Resultado: ", evaluate(num1, num2, operador))
     console.log("i: ", i)
     console.log("Operação: ", num1, operador, num2)
-    console.log("Numero Digitado:", usuarioRes)
 });
-
-
-/* let segundoAtual = 45
-document.querySelector('#segundo').innerHTML = segundoAtual
-
-setInterval(() => {
-    
-    let novoSegundo = segundoAtual--
-
-    segundoAtual.innerHTML = novoSegundo
-
-    console.log(novoSegundo)
-}, 1000); */
